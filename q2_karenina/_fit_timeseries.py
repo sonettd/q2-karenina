eee2bc07e9e411b6c7ee88efa768e2ec354c3ce8
Jesse Zaneveld <zaneveld@gmail.com>
2018-09-24 13:42:14 -0700

    Added in index template rendering in _fit_timeseries.py. Cleaned up unused imports and standardized others

--- q2_karenina/_fit_timeseries.py
+++ q2_karenina/_fit_timeseries.py
@@ -3,20 +3,17 @@ from __future__ import division
 
 __author__ = "Jesse Zaneveld"
 __copyright__ = "Copyright 2016, The Karenina Project"
-__credits__ = ["Jesse Zaneveld"]
+__credits__ = ["Jesse Zaneveld","Samuel L. Peoples"]
 __license__ = "GPL"
-__version__ = "0.0.1-dev"
+__version__ = "0.1-dev"
 __maintainer__ = "Jesse Zaneveld"
 __email__ = "zaneveld@gmail.com"
 __status__ = "Development"
 
-import karenina.fit_timeseries as k_fit_timeseries
-import pkg_resources
-import qiime2
-import q2templates
-from q2_types.ordination import PCoAResults
-import pandas as pd
-import os
+from q2templates import render
+from os.path import join
+from pkg_resources import resource_filename
+from karenina.fit_timeseries import parse_pcoa,parse_metadata
 
 def fit_timeseries(output_dir: str, pcoa : str, metadata:str, method : str, 
                 individual_col: str, timepoint_col: str, treatment_col: str) -> None:
@@ -27,14 +24,54 @@ def fit_timeseries(output_dir: str, pcoa : str, metadata:str, method : str,
     if 'None' in treatment_col:
 	    treatment_col = None
 
+
+    #readin and format      
+    site, metadata = k_fit_timeseries.parse_pcoa(pcoa, individual_col,\
+      timepoint_col, treatment_col, metadata=None)
+    model_input = k_fit_timeseries.parse_metadata(metadata, individual_col,\
+      timepoint_col, treatment_col, site)
+   
      
-    site, metadata = k_fit_timeseries.parse_pcoa(pcoa, individual_col, timepoint_col, treatment_col, metadata=metadata)
-    input = k_fit_timeseries.parse_metadata(metadata, individual_col, timepoint_col, treatment_col, site)
-    
     if treatment_col is not None:
-        output, cohort_output = k_fit_timeseries.fit_input(input, individual_col, timepoint_col, treatment_col, method)
-        cohort_output.to_csv(os.path.join(output_dir,"cohort_fit_timeseries.csv"), index=False)
+        #If there is a non-empty treatment column, then fit a cohort model,
+        # in which all individuals with the same treatment
+        #have the same model parameters.
+        
+        output, cohort_output = k_fit_timeseries.fit_input(model_input,\
+          individual_col, timepoint_col, treatment_col, method)
+        
+        result = cohort_output
+        plot_name = 'Ornstein-Uhlenbeck cohort model fit results (treatment = {},method={})'.format(treatment_col,method)
     else:
-        output = k_fit_timeseries.fit_input(input, individual_col, timepoint_col, treatment_col, method)
-    output.to_csv(os.path.join(output_dir,"individual_fit_timeseries.csv"), index=False)
+        #IF no treatment is provided, fit a model to each individual
+ 
+       result = k_fit_timeseries.fit_input(model_input, individual_col,\
+          timepoint_col, treatment_col, method)
+ 
+        plot_name = 'Ornstein-Uhlenbeck individual model fit results (method={})'.format(method)
+   
+    #Output the results (individual or cohort based model fit) to a CSV filte 
+    result.to_csv(os.path.join(output_dir,"fit_timeseries_results.csv"), index=False)
+
+    #Generate the index.html file required by QIIME2 by filling in the template
+    render_index_html(output_dir,plot_name)
+
+
+def render_index_html(output_dir,plot_name):
+
+    #Find the filepath for the q2_emperor folder 'assets'.
+    #NOTE: to understand what's happening here see e.g. stackoverflow example here:
+    #https://stackoverflow.com/questions/39104/finding-a-file-in-a-python-module-distribution
+
+    template_dir = resource_filename('q2_karenina', 'assets')
+
+    #get the path to our basic, unfilled index.html file (in the assets folder of q2_emperor)
+    index = os.path.join(template_dir, 'index.html')
+
+    #Use q2_templates.render to fill in data specific to our output in this visualization.
+    #Documentation for q2_templates.render is available here:
+    # https://github.com/qiime2/q2templates/blob/master/q2templates/_templates.py
+
+    render(index, output_dir, context={'plot_name': plot_name})
+
 
